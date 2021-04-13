f53aec919f4a0ad485194c80a07c14e71f900228
FabrizioRozzi <rozzi458@gmail.com>
2021-04-13 19:38:39 +0200

    finito settimo esercizio

--- script.js
+++ script.js
@@ -51,12 +51,62 @@
 
     // QUARTO ESERCIZIO
     
-var arrayN=[];
-for(i=0; i<6; i++){
-  var nIns=parseInt(prompt("Inserisci un numero"));
-  if(nIns%2){
-    arrayN.push(nIns);
-    console.log(arrayN)
-  }
+// var arrayN=[];
+// for(i=0; i<6; i++){
+//   var nIns=parseInt(prompt("Inserisci un numero"));
+//   if(nIns%2){
+//     arrayN.push(nIns);
+//     console.log(arrayN)
+//   }
+// }
+
+
+    // QUINTO ESERCIZIO
+
+
+// var num_utente = prompt("Inserisci un numero...")
+// var num_calcolato = 0;
+// var somma = 0;
+// var flag = false;
+
+// for(var i = 1; i<=num_utente; i++){
+//   num_calcolato = Math.pow(i, 3);
+//   console.log("Il cubo calcolato di " + i + " é: "  + num_calcolato);
+//   somma = somma + num_calcolato;
+//   console.log("La somma é: "+somma);
+ 
+// }
+
+
+
+    // // SESTO ESERCIZIO
+    // var i = 1;
+    // var num_calcolato = 0;
+    
+    // while(num_calcolato<1000){
+    //   console.log("La potenza calcolata di 2 elevato alla " + i + " é: "  + num_calcolato);   
+    //   num_calcolato = Math.pow(2, i);
+    //   i++;
+    // }
+
+
+    // SETTIMO ESERCIZIO
+var n = prompt("Inserisci un numero di 4 cifre...");
+var totale = 0;
+var addendo = 0;
+var i = 0;
+  
+for(i = 0; i < 4; i++){
+  
+  addendo = (Math.trunc(n/1000))/1;
+  console.log("sono addendo" + addendo);
+
+  totale = totale + addendo;
+  console.log("somma ciclo  "  + totale);
+
+  n = [(n - (addendo * 1000))*10];
+  console.log("sono n : " + n);
+
 }
 
+console.log("La somma delle cifre del numero inserito é: " + totale);
